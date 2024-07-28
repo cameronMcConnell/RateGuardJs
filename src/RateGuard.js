1f3e7e1ab6e1fd7d3904122224116c9f17802edf
Cameron McConnell <cameron@angry-dogman.com>
2024-07-27 20:47:38 -0500

    Refactored index.js to just export ES module for frontend and import CommonJS module for node.js testing.

---
+++ src/RateGuard.js
@@ -0,0 +1,67 @@
+class RateGuardJS {
+    #urlTotalRequests
+    #urlFirstAccessTimes
+    
+    constructor(totalAllowedRequests, totalRequestsWindowMins, timeoutMinutes) {
+        this.totalAllowedRequests = totalAllowedRequests
+        this.totalRequestsWindowMins = totalRequestsWindowMins
+        this.timeoutMinutes = timeoutMinutes
+        this.#urlTotalRequests = this.#loadUrlTotalRequests() || {}
+        this.#urlFirstAccessTimes = this.#loadUrlFirstAccessTimes() || {}
+    }
+
+    async sendRequest(url, params, requestHandler) {
+        const currentTime = Date.now()
+
+        if (!this.#urlTotalRequests[url]) {
+            this.#urlTotalRequests[url] = 0
+            this.#urlFirstAccessTimes[url] = currentTime
+            this.#saveUrlFirstAccessTimes()
+        }
+
+        const timeElapsed = this.#msToMins(currentTime - this.#urlFirstAccessTimes[url])
+
+        if (timeElapsed > this.totalRequestsWindowMins) {
+            this.#urlTotalRequests[url] = 0
+            this.#urlFirstAccessTimes[url] = currentTime
+            this.#saveUrlFirstAccessTimes()
+        }
+
+        this.#urlTotalRequests[url] += 1
+        this.#saveUrlTotalRequests()
+
+        if (this.#urlTotalRequests[url] >= this.totalAllowedRequests) {
+            await this.#delay(this.timeoutMinutes * 60 * 1000)
+        }
+
+        return requestHandler(url, params)
+    }
+
+    #msToMins(ms) {
+        return ms / 1000 / 60
+    }
+
+    #delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
+
+    #loadUrlTotalRequests() {
+        const value = localStorage.getItem("urlTotalRequests")
+        return value ? JSON.parse(value) : null
+    }
+
+    #saveUrlTotalRequests() {
+        localStorage.setItem("urlTotalRequests", JSON.stringify(this.#urlTotalRequests))
+    }
+
+    #loadUrlFirstAccessTimes() {
+        const value = localStorage.getItem("urlFirstAccessTimes")
+        return value ? JSON.parse(value) : null
+    }
+
+    #saveUrlFirstAccessTimes() {
+        localStorage.setItem("urlFirstAccessTimes", JSON.stringify(this.#urlFirstAccessTimes))
+    }
+}
+
+module.exports = RateGuardJS
